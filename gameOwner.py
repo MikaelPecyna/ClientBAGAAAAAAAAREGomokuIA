310a8a15a238d4e1f8626e32cfc51fb9f8cebe3e
Mikael Pecyna <mikael.pecyna@gmail.com>
2023-03-08 19:58:51 +0100

    initGame et isGameCreated crée

--- gameOwner.py
+++ gameOwner.py
@@ -5,4 +5,12 @@ import Client as c
 class GameOwner(c.Client):
 
     def initGame(self, type):
-        
+        self.send([Client.C_INIT_GAME, type])
+
+    def getGameId(self):
+        recv = self.receive()
+
+        if recv[0] == Client.S_GAME_CREATED and recv[1] == self.gameId:
+            print("Game created")
+        else:
+            self.catchError("Erreur lors de la creation de la partie")
